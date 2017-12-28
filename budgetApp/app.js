6a7bc779055fd87513c0e5f1deca751749e61f03
gorskim <michau666@gmail.com>
2017-12-28 16:21:16 +0100

    budgetApp after 78 lesson

--- budgetApp/app.js
+++ budgetApp/app.js
@@ -14,9 +14,15 @@ var budgetController = (function() {
         this.value = value;
     };
 
-    var allExpenses = [];
-    var allIncomes = [];
-    var totalExpenses = 0;
+    var calculateTotal = function(type) {
+        var sum = 0;
+
+        data.allItems[type].forEach(function(cur) {
+            sum = sum + cur.value;
+        });
+
+        data.totals[type] = sum;
+    };
 
     var data = {
         allItems: {
@@ -26,7 +32,9 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     }
 
     return {
@@ -54,6 +62,32 @@ var budgetController = (function() {
             return newItem;
         },
 
+        calculateBudget: function() {
+
+            // Calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // Calculate the percentage of income that we spent
+            if(data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100)
+            } else {
+                data.percentage = -1
+            }
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
+        },
+
         testing: function() {
             console.log(data);
         }
@@ -72,6 +106,10 @@ var UIController = (function() {
         inputBtn: '.add__btn',
         incomeContainter: '.income__list',
         expencesContainter: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expenseLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage',
     }
 
     return {
@@ -119,11 +157,22 @@ var UIController = (function() {
             });
 
             fieldsArr[0].focus();
+        },
+
+        displayBudget: function(obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '-';
+            }
         }
     };
-    
+ 
 })();
-
 
 // APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
@@ -144,11 +193,14 @@ var controller = (function(budgetCtrl, UICtrl) {
     var updateBudget = function() {
         
         // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
 
         // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI
-
+        UICtrl.displayBudget(budget);
+        // console.log(budget);
     };
 
     var ctrlAddItem = function() {
@@ -177,6 +229,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: 0
+            });
             setupEventListeners();
         }
     }
---
+++ budgetApp/test.js
@@ -0,0 +1,17 @@
+/*
+var i = 1;
+j = 42;
+console.log('k:' + k);
+if(true) {
+    var i = 2;
+    var k = 7;
+}
+console.log('i, j, k:' + i, j, k);
+*/
+
+console.log('1');
+var kogo = '2';
+setTimeout(function() {
+    console.log(kogo);
+}, 3000);
+console.log('3');
